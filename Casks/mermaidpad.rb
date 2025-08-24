ee1808c16eec467f770872ce7fd5a7ac03487c1d
Dave Black <656118+udlose@users.noreply.github.com>
2025-08-24 10:04:34 -0500

    Update download URL for MermaidPad

--- Casks/mermaidpad.rb
+++ Casks/mermaidpad.rb
@@ -5,7 +5,7 @@ cask "mermaidpad" do
   sha256 arm:   "f5d4d73b8990489ad2119de30d1c57ac5c846020e16f1b59815c705fb3183fde",
          intel: "64d65eabd582166b32d2d1aaf8ce49715831f4b23b5d94b90b15bbfe96f8c29e"
 
-  url "https://github.com/udlose/MermaidPad/releases/download/v#{version}/MermaidPad-#{version}-osx-#{arch}.zip"
+  url "https://github.com/udlose/MermaidPad/releases/download/v#{version}/MermaidPad-#{version}-osx-#{arch}.app.zip"
   name "MermaidPad"
   desc "Cross-platform Mermaid diagram editor built with .NET 9 + Avalonia. It uses MermaidJS to render Mermaid Diagrams."
   homepage "https://github.com/udlose/MermaidPad"
