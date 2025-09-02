853d257dbcbcb35b8ef15bca4c8765d9b56b5497
udlose <udlose@users.noreply.github.com>
2025-09-02 04:47:35 +0000

    Update MermaidPad Cask to v1.0.5

--- Casks/mermaidpad.rb
+++ Casks/mermaidpad.rb
@@ -1,9 +1,9 @@
 cask "mermaidpad" do
-  version "1.0.4"
+  version "1.0.5"
   arch arm: "arm64", intel: "x64"
 
-  sha256 arm:   "e115fb865d056d8f669bb741c0d64c073f26fda7414c78d94b0fbc838d7b5c24",
-         intel: "e1472dd0f2d988172173f598e3399648dbf791e470d2b0a689b509be81b55013"
+  sha256 arm:   "f0022952c15f8925eb4b9d93b87211cdf8f7b12d2efbc606a966dda0013e8984",
+         intel: "82041a858fb3daa60e9127008cc3e5a03d0a0cf235d2c4eea52f5c1f29237b56"
 
   url "https://github.com/udlose/MermaidPad/releases/download/v#{version}/MermaidPad-#{version}-osx-#{arch}.app.zip"
   name "MermaidPad"
